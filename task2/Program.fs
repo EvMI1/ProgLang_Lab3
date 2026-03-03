452f7f32a0343d3c654a8f8f7f103eaf2d4490ff
Михаил Фадеев <evm1t@MacBook-Air-Mihail.local>
2026-03-03 18:05:28 +0500

    add task2 with solution

---
+++ task2/Program.fs
@@ -0,0 +1,28 @@
+﻿open System
+
+let inputUserString n =
+    printfn "Заполните последовательность"
+    seq{
+        for i=1 to n do
+            printf "Введите строку: "
+            let s = Console.ReadLine()
+            yield s
+    }
+
+let shortest_string (ls: string seq) =
+    Seq.fold (fun (acc:string) (s:string) -> 
+    if s.Length < acc.Length then s else acc) 
+        (Seq.head ls) (Seq.tail ls)
+
+[<EntryPoint>]
+let main _ =
+    printf "Введите размер списка: "
+    let size = 
+        match Int32.TryParse(Console.ReadLine()) with
+        | (true, value) -> value
+        | (false, _) -> failwith "ОШИБКА: неккоретный ввод" 
+    if size <= 0 then
+        failwith "ОШИБКА: неккоректный ввод"
+    let sq = inputUserString size |> Seq.cache
+    printfn "Кратчайшая строка: %A" (shortest_string sq)
+    0
